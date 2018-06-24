28effd70bd489cd9afb1f0a4fde8a5a4c4ba80c9
outnotin <outnotin@gmail.com>
2018-06-25 01:14:41 +0700

    Added animation

---
+++ TestReact.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    setTimeout(() => {
+        $('#access-number').animateNumber({ number: 332 });
+        $('#equipment-number').animateNumber({ number: 184 });
+        $('#software-number').animateNumber({ number: 53 });
+        $('#in-progress-number').animateNumber({ number: 5 });
+        $('#ticket-raised-number').animateNumber({ number: 564 });
+        $('#total-number').animateNumber({ number: 569 });
+    }, 500);
+});
--- TestReact.jsx
+++ TestReact.jsx
@@ -14,37 +14,37 @@ class Test extends React.Component {
                <div className="col-md-2 col-sm-6 col-xs-6 num-container">
                 <div className="inner-container">
                     <span className="box-title">Access</span>
-                    <span className="box-number" id="access-number">332</span>
+                    <span className="box-number" id="access-number"></span>
                 </div>
                </div>
                <div className="col-md-2 col-sm-6 col-xs-6 num-container">
                 <div className="inner-container">
                     <span className="box-title">Equipment</span>
-                    <span className="box-number" id="equipment-number">184</span>
+                    <span className="box-number" id="equipment-number"></span>
                 </div>
                </div>
                <div className="col-md-2 col-sm-6 col-xs-6 num-container">
                 <div className="inner-container">
                     <span className="box-title">Software</span>
-                    <span className="box-number" id="software-number">53</span>
+                    <span className="box-number" id="software-number"></span>
                 </div>
                </div>
                <div className="col-md-2 col-sm-6 col-xs-6 num-container">
                 <div className="inner-container">
                     <span className="box-title">In Process</span>
-                    <span className="box-number" id="in-progress-number">5</span>
+                    <span className="box-number" id="in-progress-number"></span>
                 </div>
                </div>
                <div className="col-md-2 col-sm-6 col-xs-6 num-container">
                 <div className="inner-container">
                     <span className="box-title">Ticket Raised</span>
-                    <span className="box-number" id="ticket-raised-number">564</span>
+                    <span className="box-number" id="ticket-raised-number"></span>
                 </div>
                </div>
                <div className="col-md-2 col-sm-6 col-xs-6 num-container">
                 <div className="inner-container">
-                    <span className="box-title" id="total-number">Total</span>
-                    <span className="box-number">569</span>
+                    <span className="box-title">Total</span>
+                    <span className="box-number" id="total-number"></span>
                 </div>
                </div>
            </div>
